ff1fe37f769794ffab8f8a23a2253da760f63e89
Kerim Doruk AKINCI <kerimdorukakinci@hotmail.com>
2019-02-23 19:49:06 +0300

    Alpha Version
    Added Release Build
    
    Minor Code Fix
    
    Fix for no spintax argument
    
    Example is added
    
    Fix for repeated permutations.
    
    New Release
    
    .gitignore fix

---
+++ AdvancedCommandExecutor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+
+namespace AdvancedCommandExecutor
+{
+    class Program
+    {
+        static void PrintList(List<string> List)
+        {
+            foreach (var text in List)
+                Console.WriteLine(text);
+        }
+
+        static void StartTheProcess(string cmd)
+        {
+            Process _proc = new Process();
+            _proc.StartInfo.FileName = "cmd.exe";
+            _proc.StartInfo.Arguments = "/k " + cmd;
+            _proc.Start();
+        }
+
+        static void Main(string[] args)
+        {
+            string _input;
+            if (args.Count() == 0)
+                _input = "echo {TEST|DEMO} {1|2|3}!";
+            else
+                _input = args[0];
+
+            List<string> _allPossiblePermutations = Spintax.CreateAllPossiblePermutations(_input);
+
+            PrintList(_allPossiblePermutations);
+            Console.WriteLine("All possible permutations are listed. Do you want to start the processes.(y/n)");
+
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                foreach (var _cmd in _allPossiblePermutations)
+                    StartTheProcess(_cmd);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # AdvancedCommandExecutor
 Starts multiple processes with a single spintax command.
+
+Nested spintax also can be used.
+
+Usage:
+```
+AdvancedCommandExecutor.exe "echo Hello {world|humans|coders} !!!"
+```
+Output: 
+
+```
+echo Hello world !!!
+echo Hello humans !!!
+echo Hello coders !!!
+
+All possible permutations are listed. Do you want to start the processes.(y/n)
+
+```
